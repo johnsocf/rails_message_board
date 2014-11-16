a6c590cad35ea2c236f8337091d767d61bde54bd
Catherine Johnson <cfjohnson@earthlink.net>
2014-11-16 07:51:08 -0700

    updated model for posts to have user column and added some basic initial validation.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,39 @@
 class PostsController < ApplicationController
 
 	include PostsHelper
+	# before_filter :require_all_fields, only: [:create]
 
 	def index
 		@posts = Post.all
 	end
 
 	def show
-		@user = User.find(params[:id])
+		@user = current_user
 		@post = Post.find(params[:id])
 	end 
 
 	def new
 		@post = Post.new
+
 	end
 
 	def create
-		@post = Post.new(post_params)
+		@post = Post.create(post_params)
+		@post.user = current_user.email
 		@post.save
-		redirect_to post_path(@post)
+		if @post.id
+			redirect_to post_path(@post)
+		else 
+			redirect_to posts_path
+		end
 	end
+
+	# def require_all_fields
+	# 	@post = Post.new(post_params)
+	# 	@post.save
+	# 	unless @post.title && @post.content
+	# 		flash.notice = "please fill out the whole form"
+	# 		return false
+	# 	end
+	# end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141115230959) do
+ActiveRecord::Schema.define(version: 20141116020558) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20141115230959) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "user"
   end
 
   create_table "users", force: true do |t|
