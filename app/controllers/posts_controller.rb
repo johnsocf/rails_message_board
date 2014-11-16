8a94418f7f1a573fa2544cc0362e009e31500e98
Catherine Johnson <cfjohnson@earthlink.net>
2014-11-15 18:20:57 -0700

    added post details.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,23 @@
 class PostsController < ApplicationController
+
+	include PostsHelper
+
 	def index
 		@posts = Post.all
 	end
 
 	def show
+		@user = User.find(params[:id])
 		@post = Post.find(params[:id])
 	end 
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+		@post.save
+		redirect_to post_path(@post)
+	end
 end
